738cc3f40bfd2c477143dde33a9c22debda78e86
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-08-29 21:57:23 -0700

    Added basic of pathfinding code via bfs search

---
+++ Assets/TileMaps/Scripts/Pathfinding/Path.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Linq;
+using nickmaltbie.TileMap.Common;
+
+namespace nickmaltbie.TileMap.Pathfinding
+{
+    /// <summary>
+    /// Represent a path via recursive definition for finding a path between nodes in a graph.
+    /// </summary>
+    /// <typeparam name="V">Type of elements stored in the path</typeparam>
+    public class Path<V>
+    {
+        /// <summary>
+        /// Previous path.
+        /// </summary>
+        private Path<V> previous;
+
+        /// <summary>
+        /// Element stored at this location in the path.
+        /// </summary>
+        private V element;
+
+        public Path(V element, Path<V> previous)
+        {
+            this.previous = previous;
+            this.element = element;
+        }
+
+        /// <summary>
+        /// Get the full length of the path.
+        /// </summary>
+        public int Length()
+        {
+            int length = 1;
+            Path<V> previous = this.previous;
+            while(previous != null)
+            {
+                length += 1;
+                previous = previous.previous;
+            }
+            return length;
+        }
+
+        /// <summary>
+        /// Enumerate the full path from the start to this node (will end with this node).
+        /// </summary>
+        /// <returns>Enumerable of all elements in the path</returns>
+        public IEnumerable<V> FullPath()
+        {
+            Stack<V> stack = new Stack<V>();
+            stack.Push(element);
+            Path<V> previous = this.previous;
+            while(previous != null)
+            {
+                stack.Push(element);
+                previous = previous.previous;
+            }
+            return stack.AsEnumerable();
+        }
+    }
+}
---
+++ Assets/TileMaps/Scripts/Pathfinding/PathfindExtensions.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using System.Linq;
+using nickmaltbie.TileMap.Common;
+
+namespace nickmaltbie.TileMap.Pathfinding
+{
+    /// <summary>
+    /// Pathfinding extensions between two locations in a hex grid.
+    /// </summary>
+    public static class PathfindExtensions
+    {
+        /// <summary>
+        /// Find a path between two nodes in the graph using breadth first search.
+        /// </summary>
+        /// <param name="tileMap">Tile map to find path within.</param>
+        /// <param name="source">Starting position for path.</param>
+        /// <param name="dest">Destination of path.</param>
+        /// <param name="path">Path found between source and dest in the graph. If a path cannot be found between the
+        /// two points, this will be an empty list with no elements.</param>
+        /// <typeparam name="V">Type of coordinates within the graph.</typeparam>
+        /// <typeparam name="K">Values stored within the graph</typeparam>
+        /// <returns>True if a path can be found between the source and destination, false otherwise.</returns>
+        public static bool FindPathBFS<V, K>(ITileMap<V, K> tileMap, V source, V dest, out List<V> path)
+        {
+            // Base case if either source or dest are not in the tile map.
+            if (!tileMap.IsInMap(source) || !tileMap.IsInMap(dest))
+            {
+                // Return empty list and no path found.
+                path = new List<V>();
+                return false;
+            }
+
+            // Set of all elements that have already been searched.
+            HashSet<V> searched = new HashSet<V>();
+
+            // Queue of edges between two locations in the graph.
+            Queue<(Path<V>, V)> queue = new Queue<(Path<V>, V)>();
+
+            // Initialize queue with first element.
+            queue.Enqueue((null, source));
+
+            // While there are still tiles to search.
+            while (queue.Count > 0)
+            {
+                // dequeue front of the stack.
+                (Path<V> previousPath, V node) = queue.Dequeue();
+
+                // If the end has already been searched, continue to next edge in queue;
+                // otherwise add it to the queue.
+                if (searched.Contains(node))
+                {
+                    continue;
+                }
+                else
+                {
+                    searched.Add(node);
+                }
+
+                Path<V> pathToNode = new Path<V>(node, previousPath);
+                foreach (V neighbor in tileMap.GetNeighbors(node))
+                {
+                    // Check if we found the destination
+                    if (neighbor.Equals(dest))
+                    {
+                        // Compute full path to this node and mark path as found.
+                        path = new Path<V>(node, pathToNode).FullPath().ToList<V>();
+                        return true;
+                    }
+
+                    // if the neighbor is not already searched, add it to the queue
+                    if (searched.Contains(neighbor))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        // Add a new edge from the previous node to the neighbor
+                        queue.Enqueue((pathToNode, neighbor));
+                    }
+                }
+            }
+            // Return empty list and no path found.
+            path = new List<V>();
+            return false;
+        }
+    }
+}

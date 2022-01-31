fe31f8c3381c703e297ce0da50089462415c8c3a
Nicholas Maltbie <nick.dmalt@gmail.com>
2022-01-31 01:59:23 -0800

    Updated game to add basic menu system

---
+++ Assets/TileMaps/Scripts/UI/Actions/RebindCompositeInput.cs
@@ -0,0 +1,207 @@
+﻿// Copyright (C) 2022 Nicholas Maltbie
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace nickmaltbie.TileSet.UI.Actions
+{
+    /// <summary>
+    /// Rebind a composite set of inputs
+    /// </summary>
+    public class RebindCompositeInput : MonoBehaviour, IBindingControl
+    {
+        /// <summary>
+        /// Prefix for input mapping for saving to player preferences
+        /// </summary>
+        public const string inputMappingPlayerPrefPrefix = "Input Mapping";
+
+        /// <summary>
+        /// Composite input action being modified
+        /// </summary>
+        public InputActionReference inputAction = null;
+        /// <summary>
+        /// Menu controller related to this selected object
+        /// </summary>
+        public MenuController menuController;
+
+        /// <summary>
+        /// Rebinding group definition that includes the string name and button to control it
+        /// </summary>
+        [System.Serializable]
+        public struct RebindingGroup
+        {
+            /// <summary>
+            /// Location of descriptive name of button
+            /// </summary>
+            public UnityEngine.UI.Text bindingDisplayNameText;
+            /// <summary>
+            /// Button used to start rebinding operation
+            /// </summary>
+            public Button startRebinding;
+            /// <summary>
+            /// Text to show when waiting for player input
+            /// </summary>
+            public GameObject waitingForInputObject;
+        }
+
+        /// <summary>
+        /// Binding groups that control binding for each component of the composite
+        /// </summary>
+        public RebindingGroup[] rebindingGroups;
+
+        /// <summary>
+        /// Rebinding operation action waiting for player command to change button bindings
+        /// </summary>
+        public InputActionRebindingExtensions.RebindingOperation rebindingOperation { get; private set; }
+
+        /// <summary>
+        /// Get a readable display name for a binding index
+        /// </summary>
+        /// <param name="index">Index of binding information</param>
+        /// <returns>Human readable information of button for this binding index</returns>
+        private string GetKeyReadableName(int index) => InputControlPath.ToHumanReadableString(
+            this.inputAction.action.bindings[index].effectivePath,
+            InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+        /// <summary>
+        /// Get the input mapping player preference key from a given index
+        /// </summary>
+        /// <param name="index">Index of binding component</param>
+        public string InputMappingKey(int index) => $"{inputMappingPlayerPrefPrefix} {index} {this.inputAction.action.name}";
+
+        public void Awake()
+        {
+            // Load menu controller if not set
+            if (this.menuController == null)
+            {
+                this.menuController = this.GetComponentInParent<MenuController>();
+            }
+
+            // Load the default mapping saved to the file
+            // Start bindings at 1 as 0 is the composite start and 1 represents the first binding index
+            for (int i = 1; i <= this.rebindingGroups.Length; i++)
+            {
+                string inputMapping = PlayerPrefs.GetString(this.InputMappingKey(i), string.Empty);
+                if (!string.IsNullOrEmpty(inputMapping))
+                {
+                    this.inputAction.action.ApplyBindingOverride(i, inputMapping);
+                }
+            }
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < this.rebindingGroups.Length; i++)
+            {
+                int temp = i;
+                this.rebindingGroups[i].startRebinding.onClick.AddListener(() => this.StartRebinding(temp));
+                this.rebindingGroups[i].bindingDisplayNameText.text = this.GetKeyReadableName(i + 1);
+            }
+        }
+
+        /// <summary>
+        /// Start the rebinding process for a given component of this composite axis.
+        /// </summary>
+        /// <param name="index">Index of binding (starting at 0)</param>
+        public void StartRebinding(int index)
+        {
+            this.rebindingGroups[index].startRebinding.gameObject.SetActive(false);
+            this.rebindingGroups[index].waitingForInputObject.SetActive(true);
+            this.menuController.allowInputChanges = false;
+
+            this.inputAction.action.Disable();
+            this.inputAction.action.actionMap.Disable();
+
+            this.rebindingOperation = this.inputAction.action.PerformInteractiveRebinding(index + 1)
+                .WithControlsExcluding("<Pointer>/position") // Don't bind to mouse position
+                .WithControlsExcluding("<Pointer>/delta")    // To avoid accidental input from mouse motion
+                .WithCancelingThrough("<Keyboard>/escape")
+                .OnMatchWaitForAnother(0.1f)
+                .WithTimeout(5.0f)
+                .OnComplete(operation => this.RebindComplete(index))
+                .OnCancel(operation => this.RebindCancel(index))
+                .Start();
+        }
+
+        /// <summary>
+        /// Cancel the rebinding process for a given component of this composite axis.
+        /// </summary>
+        public void RebindCancel(int index)
+        {
+            int bindingIndex = index + 1;
+
+            this.rebindingGroups[index].bindingDisplayNameText.text = this.GetKeyReadableName(bindingIndex);
+            this.rebindingOperation.Dispose();
+
+            this.rebindingGroups[index].startRebinding.gameObject.SetActive(true);
+            this.rebindingGroups[index].waitingForInputObject.SetActive(false);
+            this.menuController.allowInputChanges = true;
+            this.inputAction.action.Enable();
+            this.inputAction.action.actionMap.Enable();
+        }
+
+        /// <summary>
+        /// Finish the rebinding process for a given component of this composite axis.
+        /// </summary>
+        /// <param name="index">Index of binding (starting at 0)</param>
+        public void RebindComplete(int index)
+        {
+            int bindingIndex = index + 1;
+            string overridePath = this.inputAction.action.bindings[bindingIndex].overridePath;
+            foreach (PlayerInput input in GameObject.FindObjectsOfType<PlayerInput>())
+            {
+                InputAction action = input.actions.FindAction(this.inputAction.name);
+                if (action != null)
+                {
+                    action.ApplyBindingOverride(bindingIndex, overridePath);
+                }
+            }
+
+            this.rebindingGroups[index].bindingDisplayNameText.text = this.GetKeyReadableName(bindingIndex);
+            this.rebindingOperation.Dispose();
+
+            PlayerPrefs.SetString(this.InputMappingKey(bindingIndex), this.inputAction.action.bindings[bindingIndex].overridePath);
+
+            this.rebindingGroups[index].startRebinding.gameObject.SetActive(true);
+            this.rebindingGroups[index].waitingForInputObject.SetActive(false);
+            this.menuController.allowInputChanges = true;
+            this.inputAction.action.Enable();
+            this.inputAction.action.actionMap.Enable();
+        }
+
+        public void ResetBinding()
+        {
+            this.inputAction.action.RemoveAllBindingOverrides();
+            for (int bindingIndex = 1; bindingIndex <= this.rebindingGroups.Length; bindingIndex++)
+            {
+                PlayerPrefs.DeleteKey(this.InputMappingKey(bindingIndex));
+            }
+        }
+
+        public void UpdateDisplay()
+        {
+            for (int index = 0; index < this.rebindingGroups.Length; index++)
+            {
+                int bindingIndex = index + 1;
+                this.rebindingGroups[index].bindingDisplayNameText.text = this.GetKeyReadableName(bindingIndex);
+            }
+        }
+    }
+}
---
+++ Assets/TileMaps/Scripts/UI/Text/TMProUGUIHyperlinks.cs
@@ -0,0 +1,189 @@
+﻿// Copyright (C) 2022 Nicholas Maltbie
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+namespace nickmaltbie.TileSet.UI.Text
+{
+    /// <summary>
+    /// This class handles basic link color behavior, supports also underline (static only)
+    /// Does not support strike-through, but can be easily implemented in the same way as the underline
+    /// 
+    /// forum post reference - https://forum.unity.com/threads/textmeshpro-hyperlinks.1091296/
+    /// 
+    /// https://github.com/EpsilonD3lta/UnityUtilities/blob/master/Scripts/Runtime/TMProUGUIHyperlinks.cs
+    /// </summary>
+    [DisallowMultipleComponent()]
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TMProUGUIHyperlinks : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField]
+        private Color32 hoveredColor = new Color32(0x00, 0x59, 0xFF, 0xFF);
+        [SerializeField]
+        private Color32 pressedColor = new Color32(0x00, 0x00, 0xB7, 0xFF);
+        [SerializeField]
+        private Color32 usedColor = new Color32(0xFF, 0x00, 0xFF, 0xFF);
+        [SerializeField]
+        private Color32 usedHoveredColor = new Color32(0xFD, 0x5E, 0xFD, 0xFF);
+        [SerializeField]
+        private Color32 usedPressedColor = new Color32(0xCF, 0x00, 0xCF, 0xFF);
+
+        private List<Color32[]> startColors = new List<Color32[]>();
+        private TextMeshProUGUI textMeshPro;
+        private Dictionary<int, bool> usedLinks = new Dictionary<int, bool>();
+        private int hoveredLinkIndex = -1;
+        private int pressedLinkIndex = -1;
+        private Camera mainCamera;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            int linkIndex = this.GetLinkIndex();
+            if (linkIndex != -1) // Was pointer intersecting a link?
+            {
+                this.pressedLinkIndex = linkIndex;
+                if (this.usedLinks.TryGetValue(linkIndex, out bool isUsed) && isUsed) // Has the link been already used?
+                {
+                    // Have we hovered before we pressed? Touch input will first press, then hover
+                    if (this.pressedLinkIndex != this.hoveredLinkIndex)
+                    {
+                        this.startColors = this.SetLinkColor(linkIndex, this.usedPressedColor);
+                    }
+                    else
+                    {
+                        this.SetLinkColor(linkIndex, this.usedPressedColor);
+                    }
+                }
+                else
+                {
+                    // Have we hovered before we pressed? Touch input will first press, then hover
+                    if (this.pressedLinkIndex != this.hoveredLinkIndex)
+                    {
+                        this.startColors = this.SetLinkColor(linkIndex, this.pressedColor);
+                    }
+                    else
+                    {
+                        this.SetLinkColor(linkIndex, this.pressedColor);
+                    }
+                }
+
+                this.hoveredLinkIndex = this.pressedLinkIndex; // Changes flow in LateUpdate
+            }
+            else
+            {
+                this.pressedLinkIndex = -1;
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            int linkIndex = this.GetLinkIndex();
+            if (linkIndex != -1 && linkIndex == this.pressedLinkIndex) // Was pointer intersecting the same link as OnPointerDown?
+            {
+                TMP_LinkInfo linkInfo = this.textMeshPro.textInfo.linkInfo[linkIndex];
+                this.SetLinkColor(linkIndex, this.usedHoveredColor);
+                this.startColors.ForEach(c => c[0] = c[1] = c[2] = c[3] = this.usedColor);
+                this.usedLinks[linkIndex] = true;
+                UnityEngine.Debug.Log($"Opening link - {linkInfo.GetLinkID()}");
+                Application.OpenURL(linkInfo.GetLinkID());
+            }
+
+            this.pressedLinkIndex = -1;
+        }
+
+        private int GetLinkIndex()
+        {
+            return TMP_TextUtilities.FindIntersectingLink(this.textMeshPro, Mouse.current.position.ReadValue(), null);
+        }
+
+        private List<Color32[]> SetLinkColor(int linkIndex, Color32 color)
+        {
+            TMP_LinkInfo linkInfo = this.textMeshPro.textInfo.linkInfo[linkIndex];
+
+            var oldVertexColors = new List<Color32[]>(); // Store the old character colors
+            int underlineIndex = -1;
+            for (int i = 0; i < linkInfo.linkTextLength; i++)
+            {
+                // For each character in the link string
+                int characterIndex = linkInfo.linkTextfirstCharacterIndex + i; // The current character index
+                TMP_CharacterInfo charInfo = this.textMeshPro.textInfo.characterInfo[characterIndex];
+                int meshIndex = charInfo.materialReferenceIndex; // Get the index of the material/subtext object used by this character.
+                int vertexIndex = charInfo.vertexIndex; // Get the index of the first vertex of this character.
+
+                // This array contains colors for all vertices of the mesh (might be multiple chars)
+                Color32[] vertexColors = this.textMeshPro.textInfo.meshInfo[meshIndex].colors32;
+                oldVertexColors.Add(new Color32[] { vertexColors[vertexIndex + 0], vertexColors[vertexIndex + 1], vertexColors[vertexIndex + 2], vertexColors[vertexIndex + 3] });
+                if (charInfo.isVisible)
+                {
+                    vertexColors[vertexIndex + 0] = color;
+                    vertexColors[vertexIndex + 1] = color;
+                    vertexColors[vertexIndex + 2] = color;
+                    vertexColors[vertexIndex + 3] = color;
+                }
+                // Each line will have its own underline mesh with different index, index == 0 means there is no underline
+                if (charInfo.isVisible && charInfo.underlineVertexIndex > 0 && charInfo.underlineVertexIndex != underlineIndex && charInfo.underlineVertexIndex < vertexColors.Length)
+                {
+                    underlineIndex = charInfo.underlineVertexIndex;
+                    for (int j = 0; j < 12; j++) // Underline seems to be always 3 quads == 12 vertices
+                    {
+                        vertexColors[underlineIndex + j] = color;
+                    }
+                }
+            }
+
+            this.textMeshPro.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
+            return oldVertexColors;
+        }
+
+        private void ResetLinkColor(int linkIndex, List<Color32[]> startColors)
+        {
+            TMP_LinkInfo linkInfo = this.textMeshPro.textInfo.linkInfo[linkIndex];
+            int underlineIndex = -1;
+            for (int i = 0; i < linkInfo.linkTextLength; i++)
+            {
+                int characterIndex = linkInfo.linkTextfirstCharacterIndex + i;
+                TMP_CharacterInfo charInfo = this.textMeshPro.textInfo.characterInfo[characterIndex];
+                int meshIndex = charInfo.materialReferenceIndex;
+                int vertexIndex = charInfo.vertexIndex;
+
+                Color32[] vertexColors = this.textMeshPro.textInfo.meshInfo[meshIndex].colors32;
+                if (charInfo.isVisible)
+                {
+                    vertexColors[vertexIndex + 0] = startColors[i][0];
+                    vertexColors[vertexIndex + 1] = startColors[i][1];
+                    vertexColors[vertexIndex + 2] = startColors[i][2];
+                    vertexColors[vertexIndex + 3] = startColors[i][3];
+                }
+
+                if (charInfo.isVisible && charInfo.underlineVertexIndex > 0 && charInfo.underlineVertexIndex != underlineIndex && charInfo.underlineVertexIndex < vertexColors.Length)
+                {
+                    underlineIndex = charInfo.underlineVertexIndex;
+                    for (int j = 0; j < 12; j++)
+                    {
+                        vertexColors[underlineIndex + j] = startColors[i][0];
+                    }
+                }
+            }
+
+            this.textMeshPro.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
+        }
+    }
+}
